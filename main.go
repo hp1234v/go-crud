a641acf4b5fe755069ec05b1f5fb4c2be56ec136
Hari Prasad <hp1234v@gmail.com>
2025-04-27 12:25:49 +0530

    added trusted proxy

--- main.go
+++ main.go
@@ -23,6 +23,13 @@ func main() {
 	initializers.DB.AutoMigrate(&models.Post{})
 
 	r := gin.Default()
+
+	// Define trusted proxies (could be the IP address of your reverse proxy)
+	r.SetTrustedProxies([]string{"127.0.0.1"})  // Example for local setup	
+
+	// Trust the proxy with a known IP address
+	// r.SetTrustedProxies([]string{"<proxy-ip>"})
+	
 	routes.PostRoutes(r)
 	r.Run()
 }
